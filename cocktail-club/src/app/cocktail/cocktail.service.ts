b3128e6da9f37816ebb2c825923a38fa2defe3e5
Nico Paulino <nicopaulino8@gmail.com>
2021-01-29 12:38:34 -0600

    Add Parameters for Get Request

--- cocktail-club/src/app/cocktail/cocktail.service.ts
+++ cocktail-club/src/app/cocktail/cocktail.service.ts
@@ -2,17 +2,17 @@ import { Injectable } from "@angular/core";
 import { Observable, of } from "rxjs";
 import { HttpClient } from "@angular/common/http";
 import { ICocktail } from "./cocktail.interface";
-
+import { HttpParams } from "@angular/common/http";
 
 @Injectable({
   providedIn: "root"
 })
 export class CocktailService {
-  //fetch a pokemon by id from https://pokeapi.co/api/v2/pokemon/{id}
 
   constructor(private http: HttpClient) {}
-  get(): Observable<ICocktail> {
-    const cocktailUrl = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=martini&1/`;
-    return this.http.get(cocktailUrl);
+  get(name: string): Observable<ICocktail> {
+    const cocktailUrl = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=martini/`;
+    const params = new HttpParams().append('s', name).append('1', '');
+    return this.http.get(cocktailUrl, {params});
   }
 }
