30236bf212ea410f510d1d917e41594e5f28d94b
Nico Paulino <nicopaulino8@gmail.com>
2021-01-29 12:16:51 -0600

    Add Cocktail Interface

---
+++ cocktail-club/src/app/cocktail/cocktail.interface.ts
@@ -0,0 +1,10 @@
+export interface ICocktail {
+    idDrink?: number;
+    strDrink?: string;
+    strVideo?: string;
+    strCategory?: string;
+    strAlcoholic?: string;
+    strGlass?: string;
+    strInstructions?: string;
+    strDrinkThumb?: string;
+  }
